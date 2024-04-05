5e169eb1fb9522183d87029fa9eefee864710d90
Shahzad1414 <shahzadkhatri@outlook.com>
2024-04-05 06:15:35 +0500

    backend added

---
+++ backend/controller/emailCtrl.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const asyncHandler = require('express-async-handler');
+
+
+const sendEmail = asyncHandler(async(data, req, res)=>{
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // Use `true` for port 465, `false` for all other ports
+        auth: {
+          user: process.env.MAIL_ID,
+          pass: process.env.MP,
+        },
+    })
+
+    //send mail with defined transport object
+
+    let info = await transporter.sendMail({
+        from: '"Hey 👻" <internetbazaarofficial@gmail.com>', // sender address
+        to: data.to, // list of receivers
+        subject: data.subject, // Subject line
+        text: data.subject, // plain text body
+        html: data.html, // html body
+    });
+
+    console.log("message sent: %s", info.messageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+})
+
+module.exports = sendEmail;
